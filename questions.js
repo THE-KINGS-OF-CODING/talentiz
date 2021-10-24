56d4ca0f4248fc38705664e9cac11f63af01e198
The Kings Of Coding <srijith2612@outlook.com>
2021-10-24 20:22:57 +0530

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,109 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "The path of the light is",
+    answer: "Always a straight line",
+    options: [
+      "Always a straight line",
+      "A curved line",
+      "A zig-zag line",
+      "Depends on the medium"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Uneven surfaces show................ reflection",
+    answer: "irregular",
+    options: [
+      "irregular",
+      "regular"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Incident ray, the reflected ray and .................. lie in the same plane",
+    answer: "normal",
+    options: [
+      "normal",
+      "different",
+    ]
+  },
+    {
+    numb: 4,
+    question: "When we stand in front of our dressing table, our left hand seems to be right and right seems to be left. This is called",
+    answer: "Lateral Inversion",
+    options: [
+      "Left-right confusion",
+      "Lateral Inversion",
+      "Up-side down phenomenon",
+      "Mirage"
+    ]
+  },
+    {
+    numb: 5,
+    question: "An erect and enlarged image can be formed by",
+    answer: "only a concave mirror",
+    options: [
+      "only a convex mirror",
+      "only a concave mirror",
+      "only a plane mirror",
+      "both convex and concave mirror"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Beam of light striking the reflecting surface is called",
+    answer: "Incident ray",
+    options: [
+      "Reflecting ray",
+      "Incident ray",
+      "Refracted ray",
+      "Normal ray"
+    ]
+  },
+  {
+    numb: 7,
+    question: "In this retina of the eye, the area having no sensory cells is called",
+    answer: "Blind Spot",
+    options: [
+      "iris",
+      "Blind spot",
+      "Cornea",
+      "Dark Spot"
+    ]
+  },
+  {
+    numb: 8,
+    question: "In case of reflection of light the angle of incidence (i) and the angle of reflection (r) are related as",
+    answer: "i = r",
+    options: [
+      "i = r",
+      "i < r",
+      "i > r",
+      "No definite relation"
+    ]
+  },
+  {
+    numb: 9,
+    question: "Visually impaired people can read and write using",
+    answer: "braile system",
+    options: [
+      "electronic writer",
+      "digital pens",
+      "braile system",
+      "hearing aids"
+    ]
+  },
+  {
+    numb: 10,
+    question: "The defect due to which a person is not able to see the distant objects clearly",
+    answer: "Myopia",
+    options: [
+      "Myopia",
+      "Hypermetropia",
+      "Cornea",
+      "Cataract"
+    ]
+  },
+];
